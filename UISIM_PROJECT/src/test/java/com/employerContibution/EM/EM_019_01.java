3f26fee367759ec7e4f6a61e0331662f2ff797c1
sudarsana.kanthasamy <sudarsana.kanthasamy@100.96.6.207>
2023-03-14 03:16:12 -0400

    Added testcase EL 322_010

--- UISIM_PROJECT/src/test/java/com/employerContibution/EM/EM_019_01.java
+++ UISIM_PROJECT/src/test/java/com/employerContibution/EM/EM_019_01.java
@@ -41,7 +41,7 @@ public class EM_019_01 extends TestBase
 	public void EM_019_01() throws Exception
 	{
 		 
-		 test = report.createTest("EM.321.02.008 - Verify Employer is able to Edit Trustee/Owner Details .");
+		 test = report.createTest("EM.019.01- Verify CSR is able to search Legal name of business and add Related Business Details of a Joint Employment.");
 		 LoginPage loginPage = PageFactory.initElements(driver, LoginPage.class);
 		 AddCorporatePage addCorporatePage = PageFactory.initElements(driver, AddCorporatePage.class);
 		 commonStepDefinitions commonFuntions= new commonStepDefinitions();
@@ -54,80 +54,60 @@ public class EM_019_01 extends TestBase
 		 commonFuntions.screenShot("Menu","Pass","Joint Employment/Management Agreement Creation");
 		 commonFuntions.clickMenu("Joint Employment/Management Agreement Creation");
 		 commonFuntions.clickButtonContains("Continue");
-		 Thread.sleep(2000);
+		 Thread.sleep(2000);		 
 		 commonFuntions.errorLabel("Required");
 		 commonFuntions.enterTextbox("Employer Registration Number","6543210" );
+		 commonFuntions.screenShot("Error_Required","Pass","Error message shown as expected");
 		 commonFuntions.clickButtonContains("Continue");
 		 Thread.sleep(2000);
 		 commonFuntions.errorContent("The Employer Registration Number(ERN) provided does not exist in the system.");
+		 commonFuntions.screenShot("Error_Required1","Pass","Error message shown as expected");
 		 commonFuntions.enterTextbox("Employer Registration Number","9300016" );
 		 commonFuntions.clickButtonContains("Continue");
 		 Thread.sleep(2000);
 		 commonFuntions.clickButtonContains("Continue");
 		 Thread.sleep(2000);
 		 commonFuntions.errorContent("To continue, you must search record and click continue button.");
+		 commonFuntions.screenShot("Error_Required2","Pass","Error message shown as expected");
 		 commonFuntions.enterTextbox("Legal Name of Business","ENSEC INC" );
+		 commonFuntions.clickButtonContains("Search");
+		 Thread.sleep(2000);
+		 commonFuntions.screenShot("Joint","Pass","Screen as expected");
 		 commonFuntions.clickButtonContains("Continue");
 		 Thread.sleep(2000);
 		 commonFuntions.errorContent("Please select a record to proceed further.");
-		 
-		 
-		 
-		 
-	     
-		 
-		 long number = commonFuntions.createRandomInteger(100000000,999999999);
-		 String ssnValue=Long.toString(number);
-	     populateFields(ssnValue);
-	     
-	     commonFuntions.clickButtonContains("Submit");
-	     Thread.sleep(2000);
-	     commonFuntions.screenShot("Success","Pass","Successfully added Trustee/Owner Details Details");
-	     addCorporatePage.successLink.click();
+		 commonFuntions.screenShot("Error_Required3","Pass","Error message shown as expected");
+		 commonFuntions.selectRadioInTable("65-0292225", 1, 1, "Joint Employment/Management Agreements");
+		 commonFuntions.screenShot("Joint1","Pass","Screen as expected");
+		 commonFuntions.clickButtonContains("Continue");
 		 Thread.sleep(2000);
-	     verifyFields(ssnValue);	
-	     ssnValue = StringUtils.left(ssnValue, 3)+"-"+StringUtils.right(StringUtils.left(ssnValue, 5),2)+"-"+StringUtils.right(ssnValue, 4);
+		 commonFuntions.selectRadio("Joint Employment");
+		 commonFuntions.selectRadioQuestions("Are businesses financially related, with the same principal(s) owning 50% or more of each business?","Yes");
+		 commonFuntions.selectRadioQuestions("Joint Employment - Are Employees concurrently employed by both businesses?","Yes");
+		 commonFuntions.selectRadioQuestions("Are businesses required to report separately for FUTA?","No");
+		 commonFuntions.populateListbox("Remarks","Test");
+		 commonFuntions.screenShot("Joint3","Pass","Screen as expected");
+		 commonFuntions.clickButtonContains("Continue");
+		 Thread.sleep(2000);
+		 commonFuntions.screenShot("Joint4","Pass","Screen as expected");
+		 commonFuntions.clickButtonContains("Continue");
+		 Thread.sleep(2000);
+		 commonFuntions.clickButtonContains("Submit");
+		 Thread.sleep(2000);
+		 commonFuntions.errorContent("Required");
+		 commonFuntions.screenShot("Error_Required4","Pass","Error message shown as expected");
+		 commonFuntions.selectDateInTable("65-0292225", 5, 1, "Joint Employment/Management Agreement Arrangement ","03/10/2023");
+		 commonFuntions.selectDateInTable("65-0292225", 6, 1, "Joint Employment/Management Agreement Arrangement ","02/10/2023");
+		 commonFuntions.screenShot("Joint5","Pass","Screen as expected");		 
+		 commonFuntions.clickButtonContains("Submit");
+		 Thread.sleep(2000);
+		 commonFuntions.errorContent("Effective end date for row 1 cannot be prior to the Effective Start Date.");
+		 commonFuntions.screenShot("Error_Required4","Pass","Error message shown as expected");
+		 
+		 
 	     
-	     commonFuntions.selectTable(ssnValue,9,1," Individual as Trustee ");
-	     Thread.sleep(2000);
-	     commonFuntions.enterTextboxContains("Address Line 1", "Updated address line 1");
-	     commonFuntions.enterTextboxContains("Address Line 2", "Updated address line 2");
-	     commonFuntions.clickButtonContains("Submit");
-	     Thread.sleep(2000);
-	     commonFuntions.screenShot("Success2","Pass","Successfully amended Trustee/Owner Details");
-	     addCorporatePage.successLink.click();
-	     Thread.sleep(2000);		
-	     
-	     String address = commonFuntions.retrieveValueFromTable(ssnValue,7,1," Individual as Trustee ");	   
-	    Assert.assertEquals(address.contains("UPDATED ADDRESS LINE 1"), true);
-	    Assert.assertEquals(address.contains("UPDATED ADDRESS LINE 2"), true);
-	     
-	   	   
+		 
+		   	   
 	}
-	public void populateFields(String ssnValue) throws Exception {
-		commonStepDefinitions commonFuntions= new commonStepDefinitions();
-		Thread.sleep(2000);
-		
-	     Random random = new Random();
-	     commonFuntions.enterTextbox("SSN ",ssnValue );
-	     commonFuntions.enterTextboxContains("First Name", "AutomationFirstName"+random.nextInt(10000));	    
-	     commonFuntions.enterTextboxContains("Last Name", "AutomationLastName"+random.nextInt(10000));
-	     commonFuntions.selectDropdown("Title", "Trustee");
-	     commonFuntions.screenShot("Populate","Pass","populate Trustee/Owner Details");	     
-	     commonFuntions.enterTextboxContains("Address Line 1", "Added address line 1");
-	     commonFuntions.enterTextboxContains("Address Line 2", "Added address line 2");
-	     commonFuntions.enterTextboxContains("City", "Added City");	    
-	     commonFuntions.enterTextboxContains("Zip", String.valueOf((long) (Math.random()*Math.pow(10,6))));
-	     commonFuntions.enterTextboxContains("Contact Number", String.valueOf((long) (Math.random()*Math.pow(10,11))));
-	     commonFuntions.screenShot("Submit","Pass","Submit Trustee/Owner Details");	
-	}
-	public void verifyFields(String ssnValue) throws InterruptedException {
-		ssnValue = ssnValue = StringUtils.left(ssnValue, 3)+"-"+StringUtils.right(StringUtils.left(ssnValue, 5),2)+"-"+StringUtils.right(ssnValue, 4);
-		commonStepDefinitions commonFuntions= new commonStepDefinitions();
-		Thread.sleep(3000);
-		String  address = commonFuntions.retrieveValueFromTable(ssnValue,7,1," Individual as Trustee ");
-		System.out.println(address);
-		 Assert.assertEquals(address.contains("ADDED ADDRESS LINE 1"), true);
-		    Assert.assertEquals(address.contains("ADDED ADDRESS LINE 2"), true);
-	}
+	
 }
