7ec0f61a4d6ec7a4ff53b884fd2078e231fcb586
vikas.singh <vikas.singh@labor.ny.gov>
2023-03-30 10:20:12 -0400

    ee package

---
+++ UISIM_PROJECT/src/test/java/com/employerContibution/EE/EE_03_001_csr_registration_emptype_business.java
@@ -0,0 +1,30 @@
+package com.employerContibution.EE;
+
+import java.util.Map;
+
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.Test;
+
+import com.aventstack.extentreports.Status;
+import com.ui.base.TestBase;
+import com.ui.pages.PEOPage;
+import com.ui.pages.employerManagement;
+import com.ui.utilities.COMMON_CONSTANT;
+
+import stepDefinitions.commonStepDefinitions;
+
+public class EE_03_001_csr_registration_emptype_business extends TestBase{
+
+	@Test()
+	public void EL_462_01_Indi_Exempt() throws Exception {
+
+		
+		commonStepDefinitions cf = new commonStepDefinitions();
+		employerManagement em =  new employerManagement();
+		
+
+
+
+
+	}
+}
