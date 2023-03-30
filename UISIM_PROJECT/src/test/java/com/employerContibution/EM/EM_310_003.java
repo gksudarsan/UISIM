5b22cb0e6af25f1c7c68a7bd6717fcee2bd661cd
vikas.singh <vikas.singh@labor.ny.gov>
2023-03-30 02:36:23 -0400

    ok

--- UISIM_PROJECT/src/test/java/com/employerContibution/EM/EM_310_003.java
+++ UISIM_PROJECT/src/test/java/com/employerContibution/EM/EM_310_003.java
@@ -24,13 +24,34 @@ public class EM_310_003 extends TestBase{
 		@Test(priority = 1, description = "Test sample", groups = { "Regression" })
 		public void EM_310() throws Exception {
 			test = report.createTest("EM.310.003 - Verify CSR is able to process sale of business when sale of business is denial.");
-			commonStepDefinitions stepDef = new commonStepDefinitions();
+			commonStepDefinitions cf= new commonStepDefinitions();
 			HomePage home = new HomePage(driver);
 			EM_005 em005 = new EM_005(driver);
 			SREG_503 sreg503 = new SREG_503(driver);
 			SREG_504 sreg504 = new SREG_504(driver);
 			SUC_002 suc002 = new SUC_002(driver);
+			test.log(Status.INFO, "Logging to the application");
+			cf.login(prop.getProperty("CSR_UserID") , prop.getProperty("CSR_Pass"));
+			test.log(Status.PASS, "Login Success");
+			test.log(Status.INFO, "Navigating to Sales of Business tab");
+			home.navigateToSaleOfBussiness();
+			test.log(Status.PASS, "Navigated to Sales of Business tab");
+			test.log(Status.INFO, "Entering the ERN Number");
+			em005.enterDetailInERNField(prop.getProperty("EAN_EM_310_02"));
+			test.log(Status.PASS, "Entered the ERN Number");
+			test.log(Status.INFO, "Entering the form data");
+			cf.selectRadio("No");
+			cf.enterTextbox("Successor", "0451789");
+			cf.clickButtonContains("Search");
+			cf.selectRadio("Part");
 			
+			cf.selectDropdown("Source", " Correspondence/Email ");sleep(2000);
+			cf.selectDropdown("Source Type", " Correspondence/Email ");sleep(2000);
+			cf.screenShot("Menu","Pass","");
+			cf.clickButtonContains("Continue");sleep();
+			cf.screenShot("Menu","Pass","");
+			test.log(Status.PASS, "Clicked on close button");
+		
 		
 		}
 	}	
