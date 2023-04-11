ec4d9245645ccc5a3abcbdaaebd1640ec079622b
abhinav.abhinav <abhinav.abhinav@host.docker.internal>
2023-04-11 03:09:01 -0400

    Corrected the Spelling mistake

--- UISIM_PROJECT/src/main/java/com/ui/base/TestBase.java
+++ UISIM_PROJECT/src/main/java/com/ui/base/TestBase.java
@@ -119,7 +119,7 @@ public class TestBase {
 			Runtime.getRuntime().exec("taskkill /F /IM chromedriver.exe /T");
 			Runtime.getRuntime().exec("taskkill /F /IM chrome.exe /T");
 			Thread.sleep(2000);
-			Process p =  Runtime.getRuntime().exec("cmd /c chrome.bat", null, new File("C:\\Users\\abhinav.abhinav\\Deskto"));
+			Process p =  Runtime.getRuntime().exec("cmd /c chrome.bat", null, new File("C:\\Users\\abhinav.abhinav\\Desktop"));
 			// Process p = Runtime.getRuntime().exec("cmd /c chrome.bat", null, new
 			// File(System.getProperty("user.dir")+"\\Driver\\chrome.bat"));
 			Thread.sleep(3000);
