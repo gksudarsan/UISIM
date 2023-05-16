3b401290764fcf16ecd55cce264c00b02f4b3077
sanjay.kumar2 <sanjay.kumar2@100.96.6.197>
2023-05-16 08:20:57 -0400

    ok

--- UISIM_PROJECT/src/test/java/com/employerContibution/EM/EM_310_05_009.java
+++ UISIM_PROJECT/src/test/java/com/employerContibution/EM/EM_310_05_009.java
@@ -38,7 +38,7 @@ public class EM_310_05_009 extends TestBase{
 		cf.clickMenu("Account Maintenance");
 		cf.screenShot("NavigateToBusinessAcquisition", "Pass", "Click on Business Acquisition");
 		cf.clickMenu("Business Acquisition");
-		sleep(2000);
+		//sleep(2000);
 		cf.screenShot("BusinessAcquisition", "Pass", "BusinessAcquisition-SREG:011");
 		cf.selectRadioQuestions("Have you acquired the business of another employer liable for New York State Unemployment Insurance?", "Yes");
 		sleep();
@@ -49,7 +49,7 @@ public class EM_310_05_009 extends TestBase{
 		String feinValue = databaseResults.get("Fein");
 		System.out.println("The Fein Value is:"+ feinValue);
 		test.log(Status.INFO, "Fein::"+"feinValue");
-		sleep(2000);
+		//sleep(2000);
 		AddPage.EmployerRegNumber.sendKeys(eanValue);
 		cf.enterRandomStringLegalName("Legal Name of Business");
 		cf.enterTextboxContains("Address Line 1", cf.createRandomInteger(10, 99) +"Cooper");
@@ -57,41 +57,41 @@ public class EM_310_05_009 extends TestBase{
 		cf.enterTextboxContains("Zip Code", cf.createRandomInteger(100, 999)+"23");
 		cf.selectRadioQuestions("Did you acquire all or part of the business?", "ALL");
 		cf.enterFutureDate("Acquisition Date", 5);
-		sleep(3000);
+		//sleep(3000);
 		cf.ScrollMenu("Business Acquisition");
-		sleep(2000);
+		//sleep(2000);
 		cf.screenShot("BusinessAcquisitionEnterDetails", "Pass", "Business Acquisition Enter Deatails");
 		cf.clickButtonContains("Continue ");
-		sleep();
+		//sleep();
 		cf.screenShot("BusinessAcquisitionDetails", "Pass", "BusinessAcquisitionDetails:SREG-012");
-		sleep();
+		//sleep();
 		cf.clickButtonContains("Submit");
-		sleep();
+		//sleep();
 		cf.screenShot("BusinessAcquisitionConfirmation", "Pass", "Business Acquisition Confirmation:SUC-002");
 		cf.clickButtonContains("Home");
-		sleep(3000);
+		//sleep(3000);
 		
 		//login with CSR account *************************
 		cf.database_UpdateQuery("UPDATE LROUIM.T_WFA_WORK_ITEM_DETAIL SET USER_ID = '"+COMMON_CONSTANT.CSR_USER_1+"' WHERE PROCESS_DETAIL_ID IN (SELECT PROCESS_DETAIL_ID FROM T_WFA_PROCESS_DETAIL WHERE FEIN='"+feinValue+"' ORDER BY UPDATED_TS desc)");
-		sleep(2000);
+		//sleep(2000);
 		cf.LogoutAndLoginIfOktaPageDisplayed(COMMON_CONSTANT.CSR_USER_1.toUpperCase(), COMMON_CONSTANT.CSR_USER_1_PASSWORD);
-		sleep(2000);
+		//sleep(2000);
 		PEOPage.queue.click();
 		cf.waitForLoadingIconToDisappear();
 		cf.enterTextboxContains("FEIN", feinValue);
 		cf.screenShot("SerachWithFein","Pass","Searching with fein number");
 		cf.clickButtonContains("Search");
-		sleep(3000);
+		//sleep(3000);
 		cf.screenShot("VerifyTransferFailedRules","Pass","Verify Transfer Failed Rules");
 		cf.clickOnLink("Verify Transfer Failed Rules");
-		sleep(3000);
+		//sleep(3000);
 		cf.clickButtonContains("Open Work Item");
 		cf.screenShot("OpenWorkItem","Pass","Open work item:WF-091");
-		sleep(3000);
+		//sleep(3000);
 		AddPage.commentBox.sendKeys("ok");
 		cf.screenShot("ValidateTotal/PartialTransferFailedRulesTask","Pass","Validate Total/Partial Transfer Failed Rules Task");
 		cf.clickButtonContains("Submit");
-		sleep(2000);
+		//sleep(2000);
 		cf.screenShot("TaskConfirmation","Pass","Task Confirmation");
 				
 	}
