56de6d36bdd6e84406875195fd6468c6c143ba46
sudarsana.kanthasamy <sudarsana.kanthasamy@100.96.6.207>
2023-05-16 03:10:50 -0400

    Added EL 441_003 and EL 462_01

--- UISIM_PROJECT/src/test/java/com/employerContibution/EL/EL_03_14.java
+++ UISIM_PROJECT/src/test/java/com/employerContibution/EL/EL_03_14.java
@@ -41,16 +41,45 @@ public class EL_03_14 extends TestBase {
 		commonFunctions.clickButtonContains("Search");
 		commonFunctions.screenShot("PeoSearchPage", "Pass", "Searched PEO List");
 		
-		commonFunctions.selectRadioInTable("787876868", 1, 1, "Search for PEO");
+		commonFunctions.selectRadioInTable("Issued", 1, 1, "Search for PEO");
 		//driver.findElement(By.xpath("//input[@id='dataTableId_selectedPeoId_1_1_radio_button-input']")).click();
-		sleep();
+		sleep(2000);
+		commonFunctions.selectLink("Document", " UPLOAD CLIENT LIST | ADD NEW CLIENTS");
+	     sleep(2000);
+	     commonFunctions.screenShot("UploadClientListPage", "Pass", "Upload CLient List Page dislaying");
+			
+	     sleep(2000);
+	     commonFunctions.clickButtonContains("ADD CLIENT DETAILS MANUALLY");
+	     sleep(2000);
+	     commonFunctions.clickButtonContains("Search");
+	     sleep(2000);
+	     commonFunctions.errorContent("Please enter Employer Registration Number or Legal Name or Federal Employer Identification number (FEIN)");
+	     commonFunctions.screenShot("verifyDefaultError","Pass","Verify Default Error");
+	     
+	     commonFunctions.enterTextboxContains("Employee Registration Number","43");
+	     
+	     commonFunctions.clickButtonContains("Search");
+	     sleep(2000);
+	     commonFunctions.errorContent("Invalid Employer Registration Number");
+	     commonFunctions.screenShot("verifyErnError","Pass","Verify ERN Error");
+	     
+	     commonFunctions.enterTextboxContains("Employee Registration Number","");
+	     
+	     commonFunctions.enterTextboxContains("Federal Employer Identification Number (FEIN)","43");
+	     
+	     commonFunctions.clickButtonContains("Search");
+	     sleep(2000);
+	     commonFunctions.errorContent("Invalid Federal Employer Identification Number (FEIN)");
+	     commonFunctions.screenShot("verifyFeinError","Pass","Verify FEIN Error");
 		
-		commonFunctions.clickButtonContains("Continue");
+		commonFunctions.enterTextboxContains("Federal Employer Identification Number (FEIN)","");	     
+	     commonFunctions.enterTextboxContains("Legal Name","test000000000");
+	     sleep(2000);		commonFunctions.clickButtonContains("Client Not Found ");
+		sleep(2000);
 		commonFunctions.screenShot("ManageGroupPeoPage", "Pass", "Group PEO Details");
 		
-		commonFunctions.clickOnLink(" UPLOAD CLIENT LIST | ADD NEW CLIENTS");
-		commonFunctions.screenShot("UploadClientListPage", "Pass", "Upload CLient List Page dislaying");
-		sleep();
+		
+		
 		
 		//step 6 ends here
 		
