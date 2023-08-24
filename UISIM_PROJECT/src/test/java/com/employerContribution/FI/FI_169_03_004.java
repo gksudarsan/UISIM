b92952f257ee086a52aa49a8b8c3e9cb9f277de0
shreya.mathur <shreya.mathur@100.96.6.17>
2023-08-24 04:18:17 -0400

    Package FI

--- UISIM_PROJECT/src/test/java/com/employerContribution/FI/FI_169_03_004.java
+++ UISIM_PROJECT/src/test/java/com/employerContribution/FI/FI_169_03_004.java
@@ -1,4 +1,4 @@
-package com.employerContibution.FI;
+package com.employerContribution.FI;
 
 import java.util.Iterator;
 import java.util.Set;
@@ -7,36 +7,25 @@ import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.Test;
 
 import com.ui.base.TestBase;
-import com.ui.pages.AddressPage;
-import com.ui.pages.COL_474;
-import com.ui.pages.FIP_006;
 import com.ui.pages.FIS_002;
-import com.ui.pages.FIS_008;
-import com.ui.pages.FIS_009;
 import com.ui.pages.PEOPage;
 import com.ui.pages.SUC_002;
 import com.ui.utilities.COMMON_CONSTANT;
 
 import stepDefinitions.commonStepDefinitions;
 
-public class FI_169_03_012 extends TestBase {
+public class FI_169_03_004 extends TestBase {
 
 	@Test
-	public void FI169_03_012() throws Exception {
+	public void FI169_03_004() throws Exception {
 		commonStepDefinitions commonFuntions = new commonStepDefinitions();
 		FIS_002 fis002 = new FIS_002(driver);
 		PEOPage PEOPage = PageFactory.initElements(driver, PEOPage.class);
 		SUC_002 suc002 = new SUC_002(driver);
 		PEOPage peopage = new PEOPage(driver);
-		COL_474 col474 = new COL_474(driver);
-		FIP_006 fip006 = new FIP_006(driver);
-		FIS_008 fis008 = new FIS_008(driver);
-		FIS_009 fis009 = new FIS_009(driver);
-
-		AddressPage AddPage = PageFactory.initElements(driver, AddressPage.class);
 
 		test = report.createTest(
-				"FI.169.03.012 - Verify Employer can submit an FI Issue when Issue Category - Protest, Issue Subcategory - ' Interest Assessment Surcharge (IAS) Protest' and system create task for CSR review");
+				"FI.169.03.004 - Verify Employer can submit an FI Issue when Issue Category - Audit ,Issue Subcategory - 'Audit Protest'  and system create task for CSR review");
 
 		commonFuntions.login(COMMON_CONSTANT.EMPLOYER_USER_8.toUpperCase(), COMMON_CONSTANT.EMPLOYER_USER_8_PASSWORD);
 		commonFuntions.waitForLoadingIconToDisappear();
@@ -59,7 +48,7 @@ public class FI_169_03_012 extends TestBase {
 		commonFuntions.selectDropdown("Category", " Protest ");
 		commonFuntions.waitForLoadingIconToDisappear();
 		sleep(1000);
-		commonFuntions.selectDropdown("Subcategory", " How do I protest an Interest Assessment Surcharge (IAS)? ");
+		commonFuntions.selectDropdown("Subcategory", " How do I protest Audit Findings? ");
 		sleep(1000);
 		commonFuntions.screenShot("Write Message", "Pass", "SM-101 with details filled screen is displayed");
 		sleep(2000);
@@ -74,75 +63,116 @@ public class FI_169_03_012 extends TestBase {
 		driver.switchTo().window(Child);
 		sleep(2000);
 		commonFuntions.screenShot("Submit Issue", "Pass", "FIS-002 screen is displayed");
-		
+
 		test.info("Step: 5 -- ");
-		String ern = commonFuntions.retrieveValue("Employer Registration Number").trim();
-		String correctERN = ern.replace("-", "");
-		
-		commonFuntions.enterTextboxContains("Specific Year Interest Assessment Surcharge being Protested", "2022");
-		commonFuntions.enterTextboxContains("Amount of the Interest Assessment Surcharge being Protested ($)", "2300");
-		fis002.reasonBasisCommentfield.sendKeys("Test Reason Of Interest");
+		//
+		sleep(2000);
+		commonFuntions.waitForLoadingIconToDisappear();
+		sleep(1000);
+		String eanvaluebefore = commonFuntions.retrieveValue("Employer Registration Number").trim();
+		String eanvalue = eanvaluebefore.replace("-", "");
+		System.out.println(eanvalue);
+		//
+		fis002.submittingIssueField.sendKeys("test comment");
+
+		//
+		fis002.quarter1.click();
+		sleep(2000);
+		fis002.quarterValue1.click();
+		sleep(2000);
+		fis002.year1.click();
+		sleep(2000);
+		fis002.yearValue1.click();
+		//
+		fis002.quarter2.click();
+		sleep(2000);
+		fis002.quarterValue2.click();
+		sleep(2000);
+		fis002.year2.click();
+		sleep(2000);
+		fis002.yearValue2.click();
+		//
+
+		commonFuntions.enterTextboxContains("Amount Protesting ($)", "10000");
+
+		commonFuntions.selectCheckbox("Were Books and records previously provided?");
+		commonFuntions.enterTextboxContains("Address Line 1",
+				commonFuntions.createRandomInteger(10, 99) + "Cooper Square");
+		commonFuntions.enterTextboxContains("City", "NY");
+		commonFuntions.enterTextboxContains("Zip Code", "13429");
+
+		commonFuntions.selectCheckbox("If 50% penalty was assessed are you protesting 50% fraud penalty?");
+		commonFuntions.selectCheckbox("Is this protest a hearing request ?");
+
 		commonFuntions.clickButtonContains("Continue ");
 		sleep(2000);
 		commonFuntions.waitForLoadingIconToDisappear();
 		commonFuntions.screenShot("Submit Issue Verification", "Pass", " screen is displayed");
-		
 		commonFuntions.clickButtonContains("Submit ");
 		commonFuntions.waitForLoadingIconToDisappear();
 		sleep(1000);
 		commonFuntions.screenShot("Issue Submission Confirmation", "Pass", "SUC-002 screen is displayed");
-	
+
 		test.info("Step: 6 -- ");
 		commonFuntions.clickButtonContains("Home");
 		sleep(2000);
 		commonFuntions.waitForLoadingIconToDisappear();
 		commonFuntions.screenShot("Home page", "Pass", "Home screen is displayed");
-		
+
 		//
-		commonFuntions.database_UpdateQuery("UPDATE LROUIM.T_WFA_WORK_ITEM_DETAIL SET USER_ID = '"+COMMON_CONSTANT.CSR_USER_1+"' WHERE PROCESS_DETAIL_ID IN (SELECT PROCESS_DETAIL_ID FROM T_WFA_PROCESS_DETAIL WHERE EAN='"+correctERN+"' ORDER BY UPDATED_TS desc);");
+		commonFuntions.database_UpdateQuery(
+				"UPDATE LROUIM.T_WFA_WORK_ITEM_DETAIL SET USER_ID = '" + COMMON_CONSTANT.CSR_USER_1
+						+ "' WHERE PROCESS_DETAIL_ID IN (SELECT PROCESS_DETAIL_ID FROM T_WFA_PROCESS_DETAIL WHERE EAN='"
+						+ eanvalue + "' ORDER BY UPDATED_TS desc);");
 		//
-		
+
 		test.info("Step: 7 -- ");
 		commonFuntions.logoutAndLogin(COMMON_CONSTANT.CSR_USER_1.toUpperCase(), COMMON_CONSTANT.CSR_USER_1_PASSWORD);
 		PEOPage.queue.click();
 		commonFuntions.waitForLoadingIconToDisappear();
 		sleep(1000);
-		
+
 		test.info("Step: 8 -- ");
 		commonFuntions.forceClearTextWithElement("Employer Registration Number");
 		sleep(1000);
-		commonFuntions.enterTextboxContains("Employer Registration Number", correctERN);
-		commonFuntions.screenShot("EanSearch", "Pass", "EanSearch");
+		commonFuntions.enterTextboxContains("Employer Registration Number", eanvalue);
+		commonFuntions.screenShot("FeinSearch", "Pass", "ernSearch");
 		commonFuntions.clickButtonContains("Search");
 		commonFuntions.waitForLoadingIconToDisappear();
 		sleep(1000);
-		
+
 		test.info("Step: 9 -- ");
-		commonFuntions.clickOnLinkfirstItem("Protest Interest Assessment Surcharge");
+		//commonFuntions.clickOnLink("Audit Protest Task"); 
+		fis002.auditProtestWorkItem.click();
 		commonFuntions.waitForLoadingIconToDisappear();
 		sleep(1000);
 		commonFuntions.screenShot("Work Item Details", "Pass", "WF-091 screen is visible");
 		Thread.sleep(2000);
-		
+
 		commonFuntions.clickButtonContains("Open Work Item ");
 		commonFuntions.waitForLoadingIconToDisappear();
 		sleep(1000);
-		commonFuntions.screenShot("Transfer Protest", "Pass", "PFP-121 screen is visible");
-		
+		commonFuntions.screenShot("Transfer Protest", "Pass", "PFP-050 screen is visible");
+
 		test.info("Step: 10 -- ");
-		commonFuntions.selectRadioQuestions("Do you want to reroute this task to another Work Basket/User?", "No ");
-		
-		fis002.commentsFieldpfp050.sendKeys("comment test");
-		commonFuntions.clickButtonContains("Submit ");
+		commonFuntions.selectRadioQuestions("Do you want to reroute this task to another work basket/User?", "No ");
+		commonFuntions.selectRadioQuestions("Do you want to create a Field Audit Request?", "No ");
+		commonFuntions.selectRadioQuestions("Do you want to add a Hold Action Flag on this account?", "No ");
+		commonFuntions.selectRadioQuestions("Is this Employer Protesting 50% Fraud Penalty?", "Yes ");
+		commonFuntions.selectRadioQuestions("Do you want to close this work item with no action taken?", "Yes ");
+        fis002.PFP020CommentFeild.sendKeys("test com");
+        commonFuntions.clickButtonContains("Submit ");
 		commonFuntions.waitForLoadingIconToDisappear();
 		sleep(1000);
 		commonFuntions.screenShot("Task Confirmation", "Pass", "SUC-002 screen is visible");
 		
 		test.info("Step: 11 -- ");
 		suc002.homeButton.click();
-		commonFuntions.waitForLoadingIconToDisappear();
 		Thread.sleep(5000);
 		commonFuntions.screenShot("Homepage", "Pass", "Homepage screen is visible");
+		
+		
+		
 		
 	}
 
--- UISIM_PROJECT/src/test/java/com/employerContribution/FI/FI_169_07_002.java
+++ UISIM_PROJECT/src/test/java/com/employerContribution/FI/FI_169_07_002.java
@@ -1,4 +1,4 @@
-package com.employerContibution.FI;
+package com.employerContribution.FI;
 
 import java.util.Iterator;
 import java.util.Map;
