313159875e4643b005cf133cd0a0136a172ceb06
ramkaran.chouhan <ramkaran.chouhan@100.96.6.231>
2023-09-18 11:31:45 -0400

    CommentingSM issue SM004.006

--- UISIM_PROJECT/src/test/java/com/employerContibution/SM/SM_004_006_CSR_CloseWithoutReplyButton.java
+++ UISIM_PROJECT/src/test/java/com/employerContibution/SM/SM_004_006_CSR_CloseWithoutReplyButton.java
@@ -11,7 +11,7 @@ import com.aventstack.extentreports.Status;
 import com.ui.base.TestBase;
 import com.ui.pages.AddressPage;
 import com.ui.pages.PEOPage;
-import com.ui.pages.smPage;
+//import com.ui.pages.smPage;
 import com.ui.utilities.COMMON_CONSTANT;
 import stepDefinitions.commonStepDefinitions;
 
@@ -23,7 +23,7 @@ public class SM_004_006_CSR_CloseWithoutReplyButton extends TestBase {
 		commonStepDefinitions cf = new commonStepDefinitions();
 		//AddressPage AddPage = PageFactory.initElements(driver, AddressPage.class);
 		PEOPage PEOPage = PageFactory.initElements(driver, PEOPage.class);
-		smPage sm = new smPage(driver);
+	//	smPage sm = new smPage(driver);
 		test = report.createTest(
 				"SM.004.006-Verify that the CSR is able to close the workflow/workitem without replying to a received message. (Close without Reply Button)");
 		cf.login(COMMON_CONSTANT.CSR_USER_1.toUpperCase(), COMMON_CONSTANT.CSR_USER_1_PASSWORD);
@@ -31,7 +31,7 @@ public class SM_004_006_CSR_CloseWithoutReplyButton extends TestBase {
 		sleep(5000);
 		PEOPage.queue.click(); 
 		cf.waitForLoadingIconToDisappear();
-		sm.filterOption.sendKeys("current");
+		//sm.filterOption.sendKeys("current");
 		cf.clickOnLinkAnchorTag("Current UI Rates");
 		sleep(3000);
 		cf.clickButtonContains("Open Work Item ");
